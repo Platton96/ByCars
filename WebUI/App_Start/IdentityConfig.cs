c528803cb0f2e2bd08e729c64838a6c69202bb13
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-05 15:38:12 +0300

    edit config password

--- WebUI/App_Start/IdentityConfig.cs
+++ WebUI/App_Start/IdentityConfig.cs
@@ -50,7 +50,7 @@ namespace WebUI
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
