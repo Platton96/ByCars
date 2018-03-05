70a3876fa9453bec56ac78a68851f77ffcbf643b
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-05 14:24:20 +0300

    add UserName for registr

---
+++ Domain/Concrete/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Domain.Concrete
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
