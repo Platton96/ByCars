84843cd413396f1e9dba798341176793991f3ef1
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-06 11:39:29 +0300

    add Nick for user

--- Domain/Entity/ApplicationUser.cs
+++ Domain/Entity/ApplicationUser.cs
@@ -7,6 +7,7 @@ namespace Domain.Entity
 {
     public class ApplicationUser: IdentityUser
     {   
+        public string Nick { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
--- WebUI/Models/AccountViewModels.cs
+++ WebUI/Models/AccountViewModels.cs
@@ -69,8 +69,8 @@ namespace WebUI.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
         [Required]
-        [Display(Name = "UserName")]
-        public string UserName { get; set; }
+        [Display(Name = "Nick")]
+        public string Nick { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
