6b2765ce2ee0d38fd591bf6ab5fdc98d7383b267
Кульбеда Сергей <sergey_kulbeda@mail.ru>
2018-03-09 12:14:59 +0300

    add ImgUrl for car and add migration

--- Domain/Entity/Car.cs
+++ Domain/Entity/Car.cs
@@ -6,6 +6,7 @@ namespace Domain.Entity
         public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
+        public string UrlImg { get; set; }
         public int Year { get; set; }
         public decimal Cost { get; set; }
         public Transmission? Transmission { get; set; }
